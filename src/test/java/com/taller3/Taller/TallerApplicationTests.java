c456b6ae70ac22466278b3c592d9ea76b44c3713
Gabriel Riaño <rianogabriel@javeriana.edu.co>
2025-05-18 19:27:18 -0500

    first commit

---
+++ src/test/java/com/taller3/Taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller3.Taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
